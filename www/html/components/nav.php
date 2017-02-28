223258dcb6b2d01723626729e6cda363401b4d77
Tim <timdonlan@gmail.com>
2017-02-27 21:57:59 -0500

    updated nav new screens

--- www/html/components/nav.php
+++ www/html/components/nav.php
@@ -39,7 +39,8 @@ $act = 'class="active"';
         <li <?php if($pg=="index.php"){echo $act;}?>><a href="./"><i class="fa fa-home"></i> <span>Home</span></a></li>
         <li <?php if($pg=="programs.php"){echo $act;}?>><a href="programs.php"><i class="fa fa-code"></i> <span>Programs</span></a></li>
         <li <?php if($pg=="pictures.php"){echo $act;}?>><a href="pictures.php"><i class="fa fa-image"></i> <span>Screenshots</span></a></li>
-        <li <?php if($pg=="remote.php"){echo $act;}?>><a href="remote.php"><i class="fa fa-arrows"></i> <span>Remote Control</span></a></li>
+        <li <?php if($pg=="camera.php"){echo $act;}?>><a href="camera.php"><i class="fa fa-arrows"></i> <span>Camera</span></a></li>
+        <li <?php if($pg=="remote2.php"){echo $act;}?>><a href="remote2.php"><i class="fa fa-arrows"></i> <span>Remote Control</span></a></li>
         <li <?php if($pg=="log.php"){echo $act;}?>><a href="log.php"><i class="fa fa-file-text-o"></i> <span>Logs</span></a></li>
         <li <?php if($pg=="message.php"){echo $act;}?>><a href="message.php"><i class="fa fa-comment"></i> <span>Messages</span></a></li>
         <li class="header">HELP</li>
